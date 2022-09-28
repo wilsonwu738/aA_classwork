9c3adaba5c915b8e9d8e6725958f14f51ed78f7b
Wilson Wu <WilsonWu@Wilsons-MacBook-Air.local>
2022-09-27 20:57:58 -0400

    practice day 2

--- week1/day2/lec2.js
+++ week1/day2/lec2.js
@@ -11,43 +11,108 @@
 //     console.log("in the else")
 // }
 
-//funtions help DRY your code, it is a procedure of code that will run when called
+// //funtions help DRY your code, it is a procedure of code that will run when called
 
-function foo(){
-    return "bar"
+// function foo(){
+//     return "bar"
+// }
+// console.log(foo())  //function invocaiton, invoking the function
+// console.log(foo)  // return what foo is, not what it returns
+
+// function foo(p1,p2){
+//     return p1+" "+p2
+// }
+// console.log(foo("wilson","wu"))
+
+// //more ways to declare a function 
+// let foo1 = function(){
+//     return "bar"
+// }
+
+// let foo2 = () =>{
+//     return "bar"
+// }
+
+// function average(num1,num2){
+//     let sum = num1+num2;
+//     let avg=sum/2;
+//     return avg;
+// }
+
+// function average2(num1,num2){
+//     let sum = sumNum(num1,num2)
+//     let avg = sum/2
+//     return avg
+// }
+
+// function sumNum(num1, num2){
+//     return num1+num2
+// }
+
+// console.log(average(1,2))
+// console.log(average2(2,5))
+
+//problem 1
+function isOdd(num){
+    if (num % 2 !== 0){
+        return true
+    }else{
+        return false
+    }
+    
 }
-console.log(foo())  //function invocaiton, invoking the function
-console.log(foo)  // return what foo is, not what it returns
 
-function foo(p1,p2){
-    return p1+" "+p2
+console.log(isOdd(-17))
+
+function isOdd2(num){
+    // num % 2 === 1
+    // return true
+    // num % 2 === 0
+    // return false
+    return num % 2 !== 0
 }
-console.log(foo("wilson","wu"))
+console.log(isOdd2(8))
 
-//more ways to declare a function 
-let foo1 = function(){
-    return "bar"
+
+//problem 2
+function plusFive(num){
+    return num + 5
 }
+console.log(plusFive(10))
 
-let foo2 = () =>{
-    return "bar"
+//problem3
+function threeOrSeven(num){
+    if (num % 3 === 0 || num % 7 === 0){
+        return true
+    } else{
+        return false
+    }
 }
+console.log(threeOrSeven(10))
 
-function average(num1,num2){
-    let sum = num1+num2;
-    let avg=sum/2;
-    return avg;
+//Problem 4
+function hello(str){
+    return "hello" + " " + str
 }
+console.log(hello("wilson"))
 
-function average2(num1,num2){
-    let sum = sumNum(num1,num2)
-    let avg = sum/2
-    return avg
+//problem 5
+function yell(string){
+    console.log(string.toUpperCase())
 }
+yell("i like coding")
 
-function sumNum(num1, num2){
-    return num1+num2
+//problem 6
+function whisper(string){
+    let copy = string.toLowerCase()
+    return copy        // why this does not work without return
 }
+console.log(whisper("I HATE MATH"))
 
-console.log(average(1,2))
-console.log(average2(2,5))
+//problem 7
+function isSubstring(searchString, subString) {
+    return searchString.indexOf(subString) !== -1
+}
+console.log(isSubstring('hello','el'))
+
+//problem 8 
