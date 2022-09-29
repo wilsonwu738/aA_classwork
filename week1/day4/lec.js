fdb9defc5911fa4047954b4a187effb69f6f6bff
Wilson Wu <WilsonWu@Wilsons-MacBook-Air.local>
2022-09-29 19:02:01 -0400

    arrays

--- week1/day4/lec.js
+++ week1/day4/lec.js
@@ -11,4 +11,25 @@ console.log(arr)
 let arr2 = ['hi', 10, 9, true, undefined, null, [1,2]];
 console.log(arr2)
 console.log(arr2[0]);
-console.log(arr2[5][1])
+console.log(arr2[6][1])
+
+//Array method
+let fruits = ["apple", "orange", "pear"];
+console.log(fruits.length);
+console.log(fruits.indexOf("pear"));
+console.log(fruits.includes("watermelon")); // includes return true or false
+
+fruits.push("watermelon");  // add the value to the end of the arrray
+console.log(fruits);
+fruits.pop();     //pop removes the last element
+console.log(fruits);
+fruits.unshift('grape');  //put the value to the front 
+console.log(fruits);
+fruits.shift()
+console.log(fruits)
+
+//more array methods, NON mutable
+console.log("immutables now")
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(fruits.concat(numbers));
+console.log(fruits)
