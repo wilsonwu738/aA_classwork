8bf5145a662d06f3a787cc51b5f82975d3e5c4b3
Wilson Wu <WilsonWu@Wilsons-MacBook-Air.local>
2022-10-06 18:55:59 -0400

    week2 day4

---
+++ week2/day4.js
@@ -0,0 +1,10 @@
+//string vs array, styling and debugging
+let str = "hello"
+str [0] = "x"
+console.log(str)
+let arr = ["h", "e", "l", 'l', 'o']
+arr[0] = 'x'
+console.log(arr)
+
+console.log(arr.join('++'))
+console.log(str.split(''))
